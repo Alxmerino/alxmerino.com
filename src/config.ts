f27a8f263dfe7c0a3b6768e3ba3ff6593c27e3b7
Rene Merino <rene@happycog.com>
2022-12-01 20:05:10 -0500

    First Commit!

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Rene Merino — Online Portfolio';
+export const SITE_DESCRIPTION = 'Senior Full-Stack Software Engineer based in Brooklyn, New York';
