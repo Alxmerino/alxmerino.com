a62531467a408ef3d0f618a6464432d8ebe20ee4
René Merino <rene@happycog.com>
2024-09-24 14:12:03 -0400

    Quick update to add more info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rene Merino — Online Portfolio';
-export const SITE_DESCRIPTION = 'Senior Full-Stack Software Engineer based in Brooklyn, New York';
+export const SITE_TITLE = 'René Merino - Senior Full-Stack Software Engineer';
+export const SITE_DESCRIPTION = 'Senior Full-Stack software engineer and problem solver specializing in crafting robust and scalable web applications.';
